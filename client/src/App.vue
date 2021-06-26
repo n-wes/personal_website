36b4038b82699a4dfdf25b2d6337b2f801114810
Nathaniel Wesley <nathanielwesley@MacBook-Pro-4.local>
2021-06-25 18:20:24 -0700

    home

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Home/>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #2e3d50;
+  
 }
 </style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello There</h1>
+    <h2>I'm Nathan Wesley.</h2>
+    <p>I’m a student studying Computer Science at Southern Methodist University in Dallas, TX. I specialize in fullstack web and mobile development.
+    </p>
+    <button>Get in touch.</button>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
