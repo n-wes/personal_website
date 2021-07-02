88f733c0ad6cc9f1986fca15d2b9a6cc207da7f2
Nathaniel Wesley <nathanielwesley@MacBook-Pro-4.local>
2021-07-01 19:38:04 -0500

    fonts

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,6 +8,18 @@ import Home from './components/Home.vue'
 </script>
 
 <style>
+/*
+  Major Mono Display
+  Imported fonts:
+  Sirin Stencil
+  Capriola
+  Josefin Sans
+*/
+@import url('https://fonts.googleapis.com/css2?family=Capriola&family=Josefin+Sans&family=Major+Mono+Display&family=Sirin+Stencil&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Sirin+Stencil&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Capriola&family=Sirin+Stencil&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Capriola&family=Josefin+Sans&family=Sirin+Stencil&display=swap');
+
 body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
